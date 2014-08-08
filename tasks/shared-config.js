1f0a6e12b365ad9bda3e411f2a2cb74683df1d2e
Andrew Wirick <awirick@gmail.com>
2014-08-08 09:33:24 -0500

    support for boolean values in css configurations.

--- tasks/shared-config.js
+++ tasks/shared-config.js
@@ -59,7 +59,7 @@ module.exports = function( grunt ) {
 		}
 
 		function getStyleSafeValue( value ) {
-			if ( ! isStringNumber( value ) && value[ 0 ] !== "#" ) {
+			if ( !isStringNumber( value ) && value[ 0 ] !== "#"	&& typeof value !== "boolean" ) {
 				value = "\"" + value + "\"";
 			}
 			return value;
@@ -190,7 +190,7 @@ module.exports = function( grunt ) {
 					sassMapStr = sassMapStr.replace( ",\n" + options.indention + ")", "\n" + options.indention + ")" );
 				}
 
-				// the slice removes the last comma 
+				// the slice removes the last comma
 				return "(" + sassMapStr.slice( 0, -1 ) + "\n)";
 			}
 			return "$" + options.name + ": " + generateSassMapsRecursive( data ).replace( /,\)/g , ")" ) + ";";
